0598cd9094d20c12542365fb06dbcdf3166a0471
rohit017 <rohit@trendlyne.com>
2019-06-09 17:59:34 +0530

    bug fix

--- CAST_codeAnalyser/settings.py
+++ CAST_codeAnalyser/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_URL = '/static/'
 
 CORS_ORIGIN_WHITELIST = (
     ['http://localhost:3000']
@@ -141,8 +142,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
