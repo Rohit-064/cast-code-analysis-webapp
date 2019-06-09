354e5ba48216f32aa42e8cbe3861af0e8707eadd
rohit017 <rohit@trendlyne.com>
2019-06-09 19:15:43 +0530

    bug fix

--- CAST_codeAnalyser/settings.py
+++ CAST_codeAnalyser/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 CORS_ORIGIN_WHITELIST = (
-    ["cast-code-analyser-webapp.herokuapp.com"]
+    ["https://cast-code-analyser-webapp.herokuapp.com/"]
 )
 
 WEBPACK_LOADER = {
